0df5768ae2e68f901e2718b7e7b453646eac5226
Syle2001ht <77475693+Mygit2001@users.noreply.github.com>
2023-04-09 22:56:27 +0700

    code login , signup form v1

--- Cinema/src/reducers/actions/Auth.js
+++ Cinema/src/reducers/actions/Auth.js
@@ -1,3 +1,4 @@
+import usersApi from "../../api/usersApi";
 import {
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
@@ -8,4 +9,39 @@ import {
     REGISTER_FAIL,
     RESET_ERROR_LOGIN_REGISTER,
   } from "../constants/Auth";
+
+  export const login = (user) => {
+    return async (dispatch, getState) => {
+      try {
+        const stateBefore = getState();
+        console.log("Todos before dispatch: ", stateBefore.authReducer);
+  
+        dispatch({
+          type: LOGIN_REQUEST,
+        });
+        const result = await usersApi.postDangNhap(user);
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ ...result.data, avtIdUser: result.data.taiKhoan })
+        );
+        dispatch({
+          type: LOGIN_SUCCESS,
+          payload: {
+            data: result.data,
+          },
+        });
+  
+        const stateAfter = getState();
+        console.log("Todos after dispatch: ", stateAfter.authReducer);
+      } catch (error) {
+        dispatch({
+          type: LOGIN_FAIL,
+          payload: {
+            error: error.response?.data ? JSON.stringify(error.response.data) : error.message,
+          },
+        });
+      }
+    };
+  };
+  
   
