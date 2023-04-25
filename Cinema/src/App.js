e2b5c31cd48963e208dc190ae74a4264d020d4cc
Syle2001ht <77475693+Mygit2001@users.noreply.github.com>
2023-04-25 13:50:14 +0700

    build components and page v2

--- Cinema/src/App.js
+++ Cinema/src/App.js
@@ -1,29 +1,42 @@
 import { Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-// import ModalTrailer from "./components/ModalTrailer";
-import LazyLoad from "./components/LazyLoad";
 
+import ModalTrailer from "./components/ModalTrailer";
+import LazyLoad from "./components/LazyLoad";
+import Loading from "./components/Loading";
 // layout
 import MainLayout from "./layouts/MainLayout";
+import AdminLayout from "./layouts/AdminLayout";
 // guards
-
+import AdminRoute from "./guards/AdminRoute";
+import CheckoutRoute from "./guards/CheckoutRoute";
+import UserProfileRoute from "./guards/UserProfileRoute";
 // page
 import Homepage from "./pages/Homepage";
-
+import BookTicket from "./pages/BookTicket";
+import MovieDetail from "./pages/MovieDetail";
+import CreateShowtime from "./pages/CreateShowtime";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
+     <Loading />
+     <ModalTrailer />
       <Suspense fallback={<LazyLoad />}>
         <Switch>
           <Route exact path={["/", "/detail/:maPhim", "/taikhoan"]}>
             <MainLayout>
               <Route exact path="/" component={Homepage} />
-            
+              <Route exact path="/detail/:maPhim" component={MovieDetail} />
             </MainLayout>
           </Route>
+          <CheckoutRoute
+                exact
+                path="/datve/:maLichChieu"
+                component={BookTicket}
+              />
           <Route
             exact
             path={["/admin/users", "/admin/movies", "/admin/showtimes", "/admin/films/addnew"]}
